02581113c0ca2187714b2c8c681a738ff08a665f
Alex <100917839+Alexandr2332@users.noreply.github.com>
2022-12-22 13:40:46 +0500

    Add files via upload

---
+++ Git/index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let a1 = 7+2,a2 = 7-3,a3 = 2*4,a4 = 4/2;
+console.log(`${a1}, ${a2}, ${a3}, ${a4}`)
+
+let height = 23, width = 10, S = height*width;
+console.log(`${S}`);
+
+let b1 = 5%3,b2 = 3%5,b3 = 5+'3',b4 = '5'-3, b5 = 75+'кг';
+console.log(`${b1}, ${b2}, ${b3}, ${b4}, ${b5}`);
+
+let heightC = 10, dC = 4, P = 3.14, v = heightC*dC*P;
+console.log(`${v}`);
+
+let a = 10, b = 2, sum = a+b, raz = a - b, proizved = a * b, del = a / b;
+console.log(`${sum}, ${raz}, ${proizved}, ${del}`); 
+
+let c = 15, d = 2, result = c+d;
+console.log(`${result}`);
+
+let am = 10, bm = 2, cm = 5, abcm = am+bm+cm;
+console.log(`${abcm}`)
+
+let cmd = 17, dmc = 10, mcd = cmd-dmc, cdm = 7, dcm = mcd+cdm;
+console.log(`${dcm}`);
+
+let name = 'Alex';
+console.log(`Привет, ${name}!`);
+
+let age = '23';
+console.log(`Мне ${age} года!`);
+
+let chas = 60*60, sutki = chas*24, mesyace = sutki*30;
+console.log(`${chas}, ${sutki}, ${mesyace}`);
+
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+console.log(`${num}`);
+
+let nam = 10;
+nam + + 1;
+nam + + 1;
+nam - - 1;
+console.log(`${nam}`);
